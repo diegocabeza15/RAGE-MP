f724405e84da408c328c4ea9640240dd561d2fd3
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2024-11-05 11:05:48 -0300

    Cambiando character

--- client_packages/character.js
+++ client_packages/character.js
@@ -1,9 +1,19 @@
-let customizationBrowser = null;
+let customizationBrowser, customizationCamera = null;
 
 mp.events.add("client:showCustomizationPanel", () => {
     if (!customizationBrowser) {
         customizationBrowser = mp.browsers.new("package://cef/character/index.html");
     }
+    // Posición de la cámara (ajusta las coordenadas según el juego)
+    const playerPosition = mp.players.local.position;
+    const cameraPosition = new mp.Vector3(playerPosition.x, playerPosition.y + 1.5, playerPosition.z + 0.5);
+
+    // Crear y configurar la cámara
+    customizationCamera = mp.cameras.new('customizationCamera', cameraPosition, new mp.Vector3(0, 0, 0), 40);
+    customizationCamera.pointAtCoord(playerPosition.x, playerPosition.y, playerPosition.z + 0.5);
+    customizationCamera.setActive(true);
+    mp.game.cam.renderScriptCams(true, false, 0, true, false); // Activa la cámara
+
     mp.gui.cursor.show(true, true);
 });
 
@@ -11,6 +21,12 @@ mp.events.add("client:hideCustomizationPanel", () => {
     if (customizationBrowser) {
         customizationBrowser.destroy();
         customizationBrowser = null;
+    }
+    if (customizationCamera) {
+        customizationCamera.setActive(false);
+        customizationCamera.destroy();
+        customizationCamera = null;
+        mp.game.cam.renderScriptCams(false, false, 0, true, false); // Regresa a la cámara predeterminada
     }
     mp.gui.cursor.show(false, false); // Oculta el cursor del mouse
 });
